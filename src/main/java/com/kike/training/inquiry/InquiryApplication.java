51720b8f30bfcbd1477b15e8a51572d277021e4b
Sanchez <esanch69@ford.com>
2025-08-05 12:29:31 +0200

    Latest minor changes

--- src/main/java/com/kike/training/inquiry/InquiryApplication.java
+++ src/main/java/com/kike/training/inquiry/InquiryApplication.java
@@ -2,9 +2,12 @@ package com.kike.training.inquiry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = { DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class })
 @EnableAspectJAutoProxy
 public class InquiryApplication {
 
